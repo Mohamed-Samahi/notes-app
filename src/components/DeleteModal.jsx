f1862f92f6fa2257b25af75ca761478b4ead8e78
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-05-21 23:46:45 +0300

    enhancing delete modal sizes

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -8,18 +8,18 @@ const DeleteModal = ({ noteId, handleDelete, setOpenDeleteModal }) => {
                     onClick={() => setOpenDeleteModal(prev => false)}
                     className="fixed top-0 left-0 w-screen h-screen bg-black cursor-pointer bg-opacity-80"
                 />
-                <div className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-43%] p-4 rounded-lg bg-white border border-yellow-3000 z-[1000] w-[30%] m-auto">
+                <div className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-43%] p-4 rounded-lg bg-white border border-yellow-3000 z-[1000] w-full sm:w-[50%] md:w-[40%] xl:w-[35%] 2xl:w-[25%] m-auto">
                     <p>Are You Sure You Want to Delete this Note?</p>
                     <div className="flex items-center justify-end gap-2 mt-10">
                         <button
                             onClick={() => setOpenDeleteModal(prev => false)}
-                            className='px-2 py-1 text-lg font-semibold text-black bg-white border border-yellow-300 rounded-lg md:text-xl md:px-4'
+                            className='px-2 py-1 text-base font-semibold text-black bg-white border border-yellow-300 rounded-lg sm:text-lg md:text-xl md:px-4'
                         >
                             cancel
                         </button>
                         <button
                             onClick={() => handleDelete(noteId)}
-                            className='px-2 py-1 text-lg font-semibold text-white bg-red-600 border border-red-600 rounded-lg md:text-xl md:px-4'
+                            className='px-2 py-1 text-base font-semibold text-white bg-red-600 border border-red-600 rounded-lg sm:text-lg md:text-xl md:px-4'
                         >
                             Delete
                         </button>
